b8fd7d51978688d19207ad759926e4216e1090da
matthieu-blin <matth.blin@gmail.com>
2021-01-26 17:32:24 +0100

    Manipulation2 : 4)

--- MultiThread.cpp
+++ MultiThread.cpp
@@ -6,17 +6,33 @@
 
 using namespace std::chrono_literals;
 
+struct V2
+{
+	float x;
+	float y;
+	void Set(float _x, float _y) { x = _x; y = _y; }
+	void Get(float& _x, float&  _y) { _x = x; _y = y; }
+};
 int main()
 {
-	std::thread t([]()
+	V2 v;
+
+	std::thread t([&v]()
 	{
-		for (int i = 0; i < 1000; ++i)
-			std::cout << i << std::endl;
+		for (int i = 0; i < 10'000'000; ++i)
+		{
+			auto f = i / 1000.f;
+			v.Set(f, f);
+		}
 	});
-	std::thread t2([]()
+	std::thread t2([&v]()
 	{
+		float x, y;
 		for (int i = 0; i < 1000; ++i)
-			std::cout << "/" << std::endl;
+		{
+			v.Get(x, y);
+			printf("x:%f, y:%f\n", x, y);
+		}
 	});
 	t.join();
 	t2.join();
