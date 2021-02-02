1aac7d181eb0f96c2001be426a8e299c4d5eb4cd
matthieu-blin <matth.blin@gmail.com>
2021-02-02 15:24:23 +0100

    5)

--- MultiThread.cpp
+++ MultiThread.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <cassert>
 
 using namespace std::chrono_literals;
 
@@ -10,8 +12,16 @@ struct V2
 {
 	float x;
 	float y;
-	void Set(float _x, float _y) { x = _x; y = _y; }
-	void Get(float& _x, float&  _y) { _x = x; _y = y; }
+	std::mutex m;
+	void Set(float _x, float _y) {
+		m.lock();
+		x = _x;
+		y = _y; 
+	}
+	void Get(float& _x, float&  _y) {
+		_x = x;
+		_y = y; 
+	}
 };
 int main()
 {
@@ -31,6 +41,8 @@ int main()
 		for (int i = 0; i < 1000; ++i)
 		{
 			v.Get(x, y);
+			if (x != y)
+				assert(false);
 			printf("x:%f, y:%f\n", x, y);
 		}
 	});
